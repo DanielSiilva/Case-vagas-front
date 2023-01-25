ed0cbf1f7d60ab06b2222d18446adda926c54a50
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-01-25 11:37:58 -0300

    Styled: add cores ao theme

--- src/styles/Themes/default.ts
+++ src/styles/Themes/default.ts
@@ -1,4 +1,19 @@
 export const defaultTheme = {
+    'White':'#FFFFFF',
+
+    'Rebecca-purple': '#663399',
+
+    'very-light-pink': '#F2F2F2',
+
+    'Dark-grey-blue': '#2F3676',
+    'Dark-blue-grey': '#191847',
+
+    'Pine-green': '#071723',
+
+    'Light-sky-blue': '#DDE3E9',
+    'Light-blue-grey': '#C5CFD6',
     
+    'Greyis-brown': '#555555'
+
     
   } as const
